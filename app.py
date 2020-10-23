9a7fd872f54d97f47a1d5f89f5885da7d5071a40
Dipesh Poudel <dipesh.poudel@extenso.com>
2020-10-23 09:18:38 +0545

    Fixed tab and space issue

--- app.py
+++ app.py
@@ -1,44 +1,45 @@
 import os
-from flask import Flask, render_template,request, redirect, url_for,flash
-import urllib.request
 from werkzeug.utils import secure_filename
-UPLOAD_FOLDER = 'static/uploads/'
+from flask import Flask, render_template, request, redirect, flash
+
+
+UPLOAD_FOLDER = '/static/uploads/'
 app = Flask(__name__)
-app.secret_key='xenon-key'
+app.secret_key = 'xenon-key'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.debug = True
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
-
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
+
 @app.route('/', methods=['POST'])
 def upload_image():
-	if 'file' not in request.files:
-		flash('No file part')
-		return redirect(request.url)
-	file = request.files['file']
-	if file.filename == '':
-		flash('No image selected for uploading')
-		return redirect(request.url)
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-        image_full_path = os.path.join(app.config['UPLOAD_FOLDER'],filename)
+    if 'cdimage' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['cdimage']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        image_full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(image_full_path)
-		#print('upload_image filename: ' + filename)
-		flash('Image successfully uploaded and displayed')
-		return render_template('index.html', filename=filename, full_path=image_full_path)
-	else:
-		flash('Allowed image types are -> png, jpg, jpeg, gif')
-		return redirect(request.url)
+        return render_template('index.html', filename=filename, full_path=image_full_path)
+    else:
+        flash('Allowed image types are -> png, jpg, jpeg, gif')
+        return redirect(request.url)
 
 
 if __name__ == '__main__':
     app.run()
+
